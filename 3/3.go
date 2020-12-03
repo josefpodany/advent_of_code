805a479a43943dcc448c75a537973c857e2e7c73
Josef Podany <josef.podany.ml@gmail.com>
2020-12-03 20:02:23 +0100

    add: slide down the woods

---
+++ 3/3.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// slideDown with the slope of dx, dy and count how
+// many trees will be hit
+func slideDown(dx, dy int, levels []string) int {
+	width := len(levels[0])
+	y, x, trees := 0, 0, 0
+
+	for {
+		x = (x + dx) % width
+		y += dy
+		if y >= len(levels) {
+			break
+		}
+
+		if rune(levels[y][x]) == '#' {
+			// oopsie!
+			trees++
+		}
+	}
+
+	return trees
+}
+
+// first element represents the dx, second is dy
+var slopes = [][]int{
+	{1, 1},
+	{3, 1},
+	{5, 1},
+	{7, 1},
+	{1, 2},
+}
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		fmt.Printf("couldn't open file: %s\n", err.Error())
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	levels := []string{}
+	for scanner.Scan() {
+		levels = append(levels, scanner.Text())
+	}
+
+	product := 1
+	for _, slope := range slopes {
+		dx, dy := slope[0], slope[1]
+		trees := slideDown(dx, dy, levels)
+		fmt.Printf("slope dx: %d, dy: %d, %d trees will be hit\n", dx, dy, trees)
+		product *= trees
+	}
+	fmt.Printf("product of all trees encountered: %d\n", product)
+}
